a24740513ff1ae9d8a4fbe087a097c45034b6899
amitpun005 <amitpun005@gmail.com>
2024-02-28 14:55:21 +0530

    first code canges push

--- src/main/java/SaloonBE/example/SaloonBE/SaloonBeApplication.java
+++ src/main/java/SaloonBE/example/SaloonBE/SaloonBeApplication.java
@@ -9,6 +9,7 @@ public class SaloonBeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SaloonBeApplication.class, args);
 		System.out.println("First push to test code changes");
+		System.out.println("AMit First push to test code changes");
 	}
 
 }
