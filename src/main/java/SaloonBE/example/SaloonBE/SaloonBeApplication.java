89e6e5767114eb2beb922d98298f35e0d27873ee
Jeevan <jeevaninthapa@gmail.com>
2024-02-28 19:54:06 +0530

    second code push

--- src/main/java/SaloonBE/example/SaloonBE/SaloonBeApplication.java
+++ src/main/java/SaloonBE/example/SaloonBE/SaloonBeApplication.java
@@ -1,5 +1,6 @@
 package SaloonBE.example.SaloonBE;
 
+import ch.qos.logback.core.CoreConstants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,7 @@ public class SaloonBeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SaloonBeApplication.class, args);
 		System.out.println("First push to test code changes");
+		System.out.println("Second Commit ");
 	}
 
 }
