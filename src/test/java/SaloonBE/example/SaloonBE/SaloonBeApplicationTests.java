29bdbeb078097791069d252affdaf3e85ad46dc0
Jeevan <jeevaninthapa@gmail.com>
2024-02-28 12:19:04 +0530

    pushing project folder

---
+++ src/test/java/SaloonBE/example/SaloonBE/SaloonBeApplicationTests.java
@@ -0,0 +1,13 @@
+package SaloonBE.example.SaloonBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaloonBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
