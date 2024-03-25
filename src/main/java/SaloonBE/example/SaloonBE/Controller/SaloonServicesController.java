6c73b836d6fd4c594d48eb0eadcadeac318e0bde
Jeevan <jeevaninthapa@gmail.com>
2024-03-25 22:06:11 +0530

    Post For Services

--- src/main/java/SaloonBE/example/SaloonBE/Controller/SaloonServicesController.java
+++ src/main/java/SaloonBE/example/SaloonBE/Controller/SaloonServicesController.java
@@ -1,11 +1,12 @@
 package SaloonBE.example.SaloonBE.Controller;
 
 import SaloonBE.example.SaloonBE.Model.Salon_Services;
+import SaloonBE.example.SaloonBE.Repository.Salon_Services_Repository;
 import SaloonBE.example.SaloonBE.Services.Saloon_Services_Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,8 +16,21 @@ public class SaloonServicesController {
     @Autowired
     private Saloon_Services_Service saloonServicesService;
 
+    @Autowired
+    private Salon_Services_Repository salonServicesRepository;
+
+    @PostMapping("serviceSave")
+    public ResponseEntity<Salon_Services> createSalonService(@RequestBody Salon_Services salon_services)
+    {
+        Salon_Services savedSalonService=salonServicesRepository.save(salon_services);
+        return new ResponseEntity<>(savedSalonService, HttpStatus.CREATED);
+    }
+
     @GetMapping("serviceList")
     public List<Salon_Services> getAllSalonsWithServices() {
         return saloonServicesService.getAllSalonsWithServices();
     }
+
+
+
 }
